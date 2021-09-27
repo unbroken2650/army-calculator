456efdf84a5d4c50c731230e7d2afecf2a24de25
한수호 Suho Han <unbroken2650@gmail.com>
2021-09-27 22:57:38 +0900

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,13 +1,13 @@
 function calc() {
-    var joinYear = Number($('#inputYear').val());
-    var joinMonth = Number($('#inputMonth').val());
-    var joinDay = Number($('#inputDay').val());
+    let joinYear = Number($('#inputYear').val());
+    let joinMonth = Number($('#inputMonth').val());
+    let joinDay = Number($('#inputDay').val());
     if (joinYear == "" || joinMonth == "" || joinDay == "") {
         alert("날짜를 정확히 선택해주세요.");
         return;
     }
      //연도 월 일 각각 알려주기
-    var joinDate = new Date(joinYear, joinMonth - 1, joinDay);
+    const joinDate = new Date(joinYear, joinMonth - 1, joinDay);
     /*1년 6개월 추가*/
     if (joinMonth <= 6) {
         joinYear += 1;
@@ -56,12 +56,12 @@ function calc() {
         joinMonth = 3;
     }
 
-    var dischargeDate = new Date(joinYear, joinMonth-1, joinDay);
+    const dischargeDate = new Date(joinYear, joinMonth-1, joinDay);
     $("#dischargeDay").html("<p>예상 전역일은 <b>" + dischargeDate.getFullYear() + "년 " + Number(dischargeDate.getMonth()+1) + "월 " + dischargeDate.getDate() + "일</b>이네요!</p>");
-    var currentDate = new Date();
-    var totalDays = parseInt((dischargeDate.getTime() - joinDate.getTime())/(1000*60*60*24));
-    var leftDays = parseInt((dischargeDate.getTime() - currentDate.getTime())/(1000*60*60*24));
-    var leftPercent = parseInt((totalDays - leftDays)/totalDays*10000)/100;
+    const currentDate = new Date();
+    const totalDays = parseInt((dischargeDate.getTime() - joinDate.getTime())/(1000*60*60*24));
+    const leftDays = parseInt((dischargeDate.getTime() - currentDate.getTime())/(1000*60*60*24));
+    const leftPercent = parseInt((totalDays - leftDays)/totalDays*10000)/100;
     if (leftPercent >= 100) {$("#percentLeft").html("<p>전역을 축하드립니다!</p>");}
     else if (leftPercent < 0) {$("#percentLeft").html("<p>입대 이전입니다.</p>");}
     else {$("#percentLeft").html("<p>오늘을 기준으로 "+leftPercent + "% 지났습니다.</p>");}
