a8a50e8e6a1b7e1ae8424c7cd08914f811a58eeb
한수호 Suho Han <58286965+unbroken2650@users.noreply.github.com>
2021-10-01 04:43:05 +0000

    design formatted and simple issues fixed

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 function calc() {
-    var joinYear = Number($('#inputYear').val());
-    var joinMonth = Number($('#inputMonth').val());
-    var joinDay = Number($('#inputDay').val());
-    if (joinYear == "" || joinMonth == "" || joinDay == "") {
+    var joinYear = Number($("#inputYear").val());
+    var joinMonth = Number($("#inputMonth").val());
+    var joinDay = Number($("#inputDay").val());
+    if (joinYear === "" || joinMonth === "" || joinDay === "") {
         alert("날짜를 정확히 선택해주세요.");
         return;
     }
@@ -19,7 +19,7 @@ function calc() {
     }
 
     /*말일 계산*/
-    if (joinDay == 1) {
+    if (joinDay === 1) {
         joinMonth -= 1;
         switch (joinMonth) {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
@@ -33,20 +33,20 @@ function calc() {
     else { joinDay -= 1;}
     /* 2월 29, 30, 31일 처리*/
     /* 2월 29일 - 윤년/평년 처리*/
-    if (joinMonth == 2 && joinDay == 29) {
-        if (joinYear % 4 == 0) {
-            if (joinYear % 100 == 0) {
-                if (joinYear % 400 != 0) {
+    if (joinMonth === 2 && joinDay === 29) {
+        if (joinYear % 4 === 0) {
+            if (joinYear % 100 === 0) {
+                if (joinYear % 400 !== 0) {
                     joinMonth = 3; joinDay = 1;
                 }
             }
         }
         else { joinMonth = 3; joinDay = 1; }
     }
-    if (joinMonth == 2 && joinDay == 30) {
-        if (joinYear % 4 == 0) {
-            if (joinYear % 100 == 0) {
-                if (joinYear % 400 != 0) {
+    if (joinMonth === 2 && joinDay === 30) {
+        if (joinYear % 4 === 0) {
+            if (joinYear % 100 === 0) {
+                if (joinYear % 400 !== 0) {
                     joinDay = 2;
                 }
             }
@@ -59,9 +59,9 @@ function calc() {
     var dischargeDate = new Date(joinYear, joinMonth-1, joinDay);
     $("#dischargeDay").html("<p>예상 전역일은 <b>" + dischargeDate.getFullYear() + "년 " + Number(dischargeDate.getMonth()+1) + "월 " + dischargeDate.getDate() + "일</b>이네요!</p>");
     var currentDate = new Date();
-    var totalDays = parseInt((dischargeDate.getTime() - joinDate.getTime())/(1000*60*60*24));
-    var leftDays = parseInt((dischargeDate.getTime() - currentDate.getTime())/(1000*60*60*24));
-    var leftPercent = parseInt((totalDays - leftDays)/totalDays*10000)/100;
+    var totalDays = parseInt((dischargeDate.getTime() - joinDate.getTime())/(1000*60*60*24), 10);
+    var leftDays = parseInt((dischargeDate.getTime() - currentDate.getTime())/(1000*60*60*24), 10);
+    var leftPercent = parseInt((totalDays - leftDays)/totalDays*10000, 10)/100;
     if (leftPercent >= 100) {$("#percentLeft").html("<p>전역을 축하드립니다!</p>");}
     else if (leftPercent < 0) {$("#percentLeft").html("<p>입대 이전입니다.</p>");}
     else {$("#percentLeft").html("<p>오늘을 기준으로 "+leftPercent + "% 지났습니다.</p>");}
